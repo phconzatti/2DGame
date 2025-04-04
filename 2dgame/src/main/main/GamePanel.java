378b876f704a2de4d653c249853077587469361b
Pedro Henrique Schneider Conzatti <phconzatti@hotmail.com>
2025-04-04 12:54:56 -0300

    Key Handler

--- 2dgame/src/main/main/GamePanel.java
+++ 2dgame/src/main/main/GamePanel.java
@@ -2,6 +2,8 @@ package main;
 
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
 import javax.swing.JPanel;
 
 public class GamePanel extends JPanel implements Runnable{
@@ -17,14 +19,22 @@ public class GamePanel extends JPanel implements Runnable{
     final int screenWidth = tileSize * maxScreenCol; // 768 pixels
     final int screenHeight = tileSize * maxScreenRow; // 576 pixels
 
+    KeyHandler keyH = new KeyHandler();
     Thread gameThread;
 
+
+    // Set player´s default position
+    int playerX = 100;
+    int playerY = 100;
+    int playerSpeed = 4;
 
     public GamePanel() {
 
         this.setPreferredSize(new Dimension(screenWidth, screenHeight));
         this.setBackground(Color.BLACK);
         this.setDoubleBuffered(true);
+        this.addKeyListener(keyH);
+        this.setFocusable(true);
     }
 
     public void startGameThread () {
@@ -36,6 +46,34 @@ public class GamePanel extends JPanel implements Runnable{
     @Override
     public void run() {
         
-        
+        while (gameThread != null) {
+
+            // Update information
+            update();
+
+            // Draw the screen with the updated information
+            repaint();
+        }
+    }
+
+    public void update () {
+
+        if (keyH.upPressed == true) {
+            playerY -= playerSpeed;
+        }
+
+    }
+
+    public void paintComponent (Graphics g) {
+
+        super.paintComponent(g);
+
+        Graphics2D g2 = (Graphics2D) g;
+
+        g2.setColor(Color.white);
+
+        g2.fillRect(playerX, playerY, tileSize, tileSize);
+
+        g2.dispose();
     }
 }
